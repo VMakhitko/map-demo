549ed4162ac5a5f2be2beb9b01897682b458d48c
Volodymyr Makhitko <volodymyr.makhitko@globallogic.com>
2015-08-17 18:19:46 +0300

    [DRAFT][map-demo] Add ConfigurationExampleActivity
    Signed-off-by: Volodymyr Makhitko <volodymyr.makhitko@globallogic.com>

---
+++ app/src/main/java/com/gis/map/ConfigureExampleActivity.java
@@ -0,0 +1,16 @@
+package com.gis.map;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
+
+/**
+ * Created by x0158990 on 17.08.15.
+ */
+public class ConfigureExampleActivity extends FragmentActivity {
+    private TextView infoTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {}
+
+}
